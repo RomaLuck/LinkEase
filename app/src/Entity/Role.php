f276d0b2322f243f426c1959b5e8fa60cfc6e950
RomaLuck <romeo91222@gmail.com>
2024-05-02 20:50:23 +0200

    add roles

---
+++ app/src/Entity/Role.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Src\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'roles')]
+class Role
+{
+    #[ORM\Id]
+    #[ORM\Column(type: 'integer')]
+    #[ORM\GeneratedValue]
+    private int $id;
+
+    #[ORM\Column(type: 'string')]
+    private string $name;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+}
